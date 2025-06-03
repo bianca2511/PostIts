3f62adf8c832f342de6f88d6d82c6415420a3f3d
Bianca Eni <b.b.eni@student.tue.nl>
2025-06-03 17:44:19 +0200

    prettier and flexible design

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -3,7 +3,7 @@ import '../styles/Input.css'
 export default function Input() {
     return(
     <div className='input-box'>
-        <h3>How was your week?</h3>
+        <h2 className='input-prompt'>How was your week?</h2>
         <textarea rows="15" cols="45" placeholder="Today was a great day!" autoFocus className='input-field'></textarea>
         <button type="submit" className="submit-button">Submit</button>
     </div>
