30597a35b95f601ead7b5039ff34010779f66a88
Bianca Eni <b.b.eni@student.tue.nl>
2025-06-03 16:46:26 +0200

    grid works!! only on one screen size :(

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -3,7 +3,8 @@ import '../styles/Input.css'
 export default function Input() {
     return(
     <div className='input-box'>
-        <textarea rows="10" cols="50" placeholder="Today was a great day!" autoFocus className='input-field'></textarea>
+        <h3>How was your week?</h3>
+        <textarea rows="15" cols="45" placeholder="Today was a great day!" autoFocus className='input-field'></textarea>
         <button type="submit" className="submit-button">Submit</button>
     </div>
 )}
