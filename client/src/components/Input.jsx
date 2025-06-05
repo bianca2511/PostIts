5d3493c6e691b044b848134a4dbf88731bf61707
Bianca Eni <b.b.eni@student.tue.nl>
2025-06-05 15:01:01 +0200

    add color circles

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -14,11 +14,11 @@ export default function Input() {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ username: "bianca4", content: content })
+            body: JSON.stringify({ username: "bianca6", content: content, color:color })
         });
 
         if (response.ok) {
-            const result = await response.json();
+            const result = await response;
             console.log("Successfully sent", result);
             setContent("");
         } else {
@@ -26,10 +26,21 @@ export default function Input() {
         }
     }
 
+    // const [color, setColor] = useState('pink');
+
     return (
         <div className='input-box'>
             <h2 className='input-prompt'>How was your week?</h2>
             <textarea rows="15" cols="45" placeholder="Today was a great day!" autoFocus value={content} onChange={(e) => setContent(e.target.value)} className='input-field'></textarea>
+            <div className='color-picker'>
+                {['pink', 'orange', 'yellow', 'green', 'blue', 'purple'].map(color => (
+                    <div
+                        key={color}
+                        className={`color-circle ${color}`}
+                        // onClick={setColor(color)}
+                    ></div>
+                ))}
+            </div>
             <button type="submit" className="submit-button" onClick={submitText}>Submit</button>
         </div>
     )
