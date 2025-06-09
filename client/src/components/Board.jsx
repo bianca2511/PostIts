a06b08faeb36b7d8314ece9bdcb111b288be62b9
Bianca Eni <b.b.eni@student.tue.nl>
2025-06-09 15:59:25 +0200

    added dialog when clicking note

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import Note from './Note'
 import Input from './Input';
 import '../styles/Board.css'
 
 export default function Board() {
     const [notes, setNotes] = useState({});
+    const noteDialogRef = useRef(null); // create a reference to the dialog node in the DOM
 
     const reloadNotes = () => {
         fetch('http://localhost:3000/api/notes')
@@ -24,17 +25,28 @@ export default function Board() {
                 console.error("Fetch error: ", error);
             });
     }
-
     useEffect(reloadNotes, []);
+
+    function toggleExpandedNote() {
+        if(!noteDialogRef.current) {
+            return;
+        }
+
+        noteDialogRef.current.hasAttribute("open") ? noteDialogRef.current.close() : noteDialogRef.current.showModal();
+    }
 
     return (
         <div className='note-board'>
             <Input reloadNotes={reloadNotes}></Input>
             <div className='notes'>
                 {Object.entries(notes).map(([username, { content, color, submissionDate }]) => (
-                    <Note key={username} username={username} content={content} color={color} submissionDate={submissionDate}></Note>
+                    <Note key={username} onClick={toggleExpandedNote} username={username} content={content} color={color} submissionDate={submissionDate}></Note>
                 ))}
             </div>
+
+        <dialog ref={noteDialogRef}>
+            Hello Word!
+        </dialog>
         </div>
     );
 }
--- client/src/components/Note.jsx
+++ client/src/components/Note.jsx
@@ -1,13 +1,14 @@
 import '../styles/Note.css'
 
-function Note({ username, content, color, submissionDate }) {
+function Note({ username, content, color, submissionDate, onClick }) {
 
     const formattedDate = new Intl.DateTimeFormat('en-NL', {
         dateStyle: "full"
     }).format(new Date(submissionDate));
 
     return (
-        <div className={`note ${color}`}>
+        // pass the onClick method to the div itself, not the whole component in Board
+        <div className={`note ${color}`} onClick={onClick}>
             <div className='note-pin'></div>
             <h2 className="note-author">{username}</h2>
             <p className="note-content">{content}</p>
