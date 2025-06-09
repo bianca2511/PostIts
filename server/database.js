2ceedd047cd65d84587df9235a3275c29872c827
Bianca Eni <b.b.eni@student.tue.nl>
2025-06-09 15:14:34 +0200

    Clear notes is the week changes

--- server/database.js
+++ server/database.js
@@ -1,13 +1,15 @@
 const sqlite = require('sqlite3').verbose();
-const db = new sqlite.Database('notes3.db');
+const db = new sqlite.Database('notes.db');
 
 db.serialize(() => {
     db.run(`
     CREATE TABLE IF NOT EXISTS notes (
-      username TEXT PRIMARY KEY,
+      username TEXT,
       content TEXT,
       color TEXT,
-      date DATE
+      date DATE,
+      currentWeekStart TEXT,
+      PRIMARY KEY (username, currentWeekStart)
     )
 `);
 });
--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ function getCurrentWeekStart() {
 }
 
 let currentStart = getCurrentWeekStart();
-console.log(currentStart);
+// console.log("Curent start:", currentStart);
 
 app.use((req, res, next) => {
     let thisWeekStart = getCurrentWeekStart();
@@ -26,56 +26,61 @@ app.use((req, res, next) => {
         currentStart = thisWeekStart;
         console.log("New week ahead :)");
     }
+
+    req.currentWeekStart = thisWeekStart; // attach current week to request so it is not needed to calculate again later
     next();
 })
 
 // Get all notes
 app.get("/api/notes", (req, res) => {
+    // Only select the notes from the current week
+    db.all(`SELECT * FROM notes WHERE currentWeekStart = ?`, [currentStart], (err, rows) => {
+            if (err) {
+                console.error('DB read error:', err);
+                return res.status(500).json({ error: 'Failed to load notes' });
+            }
 
-    db.all(`SELECT * FROM notes`, [], (err, rows) => {
-        if (err) {
-            console.error('DB read error:', err);
-            return res.status(500).json({ error: 'Failed to load notes' });
-        }
-
-        const notes = {};
-        rows.forEach(row => {
-            notes[row.username] = {
-                content: row.content,
-                color: row.color,
-                submissionDate: row.date
-            };
+            const notes = {};
+            rows.forEach(row => {
+                notes[row.username] = {
+                    content: row.content,
+                    color: row.color,
+                    submissionDate: row.date,
+                    currentWeekStart: row.currentWeekStart
+                };
+            });
+            console.log(notes);
+            res.json(notes);
         });
-
-        res.json(notes);
     });
-});
 
 
-// Post a new note
-app.post("/api/notes", (req, res) => {
-    let { username, content, color, submissionDate } = req.body;
-    console.log("Received:", { username, content, color, submissionDate });
+    // Post a new note
+    app.post("/api/notes", (req, res) => {
+        let { username, content, color, submissionDate } = req.body;
+        let currentWeekStart = req.currentWeekStart;
+        console.log("Received:", { username, content, color, submissionDate });
 
 
-    if (!username || !content || username === "" || content === "") {
-        return res.status(400).json({ error: "Missing username or content" });
-    }
-
-    const insertQuery = `
-        INSERT INTO notes (username, content, color, date)
-        VALUES (?, ?, ?, ?)
-        `;
-    db.run(insertQuery, [username, content, color, submissionDate], function (err) {
-        if (err) {
-            console.error('DB error:', err);
-            return res.status(500).json({ error: 'Database error' });
+        if (!username || !content || username === "" || content === "") {
+            return res.status(400).json({ error: "Missing username or content" });
         }
-        // notes.set(you, yourEntry, new Date().toISOString());
-        res.send("Note received sucessfully :), safely stored on my server");
-    });
-});
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+        const insertQuery = `
+        INSERT INTO notes (username, content, color, date, currentWeekStart)
+        VALUES (?, ?, ?, ?,?)
+        `;
+        // let currentWeekStart = getCurrentWeekStart(); // Get currentWeekStart when the request is made
+        // Add current week to the database
+        db.run(insertQuery, [username, content, color, submissionDate, currentWeekStart], function (err) {
+            if (err) {
+                console.error('DB error:', err);
+                return res.status(500).json({ error: 'Database error' });
+            }
+            res.send("Note received sucessfully :), safely stored on my server");
+        });
+    });
+
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`);
+    });
