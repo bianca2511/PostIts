a65a6704368a40b6523571b61c95abe31b44bbd0
Bianca Eni <b.b.eni@student.tue.nl>
2025-09-10 21:47:23 +0200

    prettify login page

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from "jwt-decode";
-import Input from './Input';
+import "../styles/Login.css"
 
-function LoginPage({success}) {
+function LoginPage({ success }) {
     const responseMessage = (response) => {
         console.log(response);
         const jwtoken = response.credential;
         const decoded = jwtDecode(jwtoken);
         let user = {
             email: decoded.email,
-            name:decoded.given_name
+            name: decoded.given_name
         }
         success(user);
     };
@@ -19,10 +19,17 @@ function LoginPage({success}) {
         alert("Login Failed! Try again :)");
     };
     return (
-        <div>
-            <h2>React Google Login</h2>
-            <br />
-            <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+        <div className='login-page'>
+            <div className='login-box'>
+                <div className='login-pin'></div>
+                <h1 className='greeting'>Welcome fellow Brussel Sprout!</h1>
+                <h3 className='greeting'>please sign in below to use the post its</h3>
+                <GoogleLogin onSuccess={responseMessage} onError={errorMessage}
+                    theme="outline"   // "filled_blue" | "filled_black" | "outline"
+                    size="large"
+                    shape="pill"
+                    width="300" />
+            </div>
         </div>
     )
 }
